f8cfc2d0c74cb345e6399eef19911c427927bd9b
Amon Tokoro <amontokoro@amons-mbp-2.home>
2021-01-08 21:50:47 -0500

    implemented orders

--- proshop-backend/controllers/productController.js
+++ proshop-backend/controllers/productController.js
@@ -5,25 +5,22 @@ import Product from '../models/productModel.js';
 // @route GET /api/products
 // @access Publi
 const getProducts = asyncHandler(async (req, res) => {
-    const products =  await Product.find({});
-    res.json(products);
-})
+  const products = await Product.find({});
+  res.json(products);
+});
 
 // @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
 const getProductById = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Product not found');
-    }
-})
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
 
-export {
-    getProducts,
-    getProductById,
-}
+export { getProducts, getProductById };
--- proshop-backend/server.js
+++ proshop-backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -15,11 +16,12 @@ app.use(express.json());
 connectDB();
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+  res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
