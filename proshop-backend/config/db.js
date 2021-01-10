d0c37ca5769e496841a16dff5aa39c78ea38135c
Amon Tokoro <amontokoro@amons-mbp-2.home>
2021-01-10 16:20:16 -0500

    implemented admin product create & delete

--- proshop-backend/config/db.js
+++ proshop-backend/config/db.js
@@ -4,18 +4,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-
-        })
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch(err) {
-        console.log(`Error: ${err.message}`);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
